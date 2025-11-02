6d1b2dc8209d68e943fa5c4d6761c06d291fa1c3
M0G1WARA <121844050+M0G1WARA@users.noreply.github.com>
2025-11-01 19:22:23 -0600

    Refactor Camera

---
+++ Scenes/Camera/camera.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+@export var move_speed: float = 300.0
+@export var zoom_speed: float = 0.1
+@export var min_zoom: Vector2 = Vector2(0.5, 0.5)
+@export var max_zoom: Vector2 = Vector2(4.0, 4.0)
+
+func _process(delta):
+	var input_dir = Vector2.ZERO
+
+	input_dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_dir = input_dir.normalized()
+
+	position += input_dir * move_speed * delta
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			zoom += Vector2(zoom_speed, zoom_speed)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			zoom -= Vector2(zoom_speed, zoom_speed)
+			
+	zoom = zoom.clamp(min_zoom, max_zoom)
--- Scenes/Grave/grave.gd
+++ Scenes/Grave/grave.gd
@@ -1,31 +1,27 @@
 extends StaticBody2D
 
 
-var player_inside := false
 var object_id: int
 
 func _ready():
 	object_id = Global.get_new_id()
 
-func _on_area_2d_body_entered(body):
-	if body is CharacterBody2D:
-		$Label.show()
-		player_inside = true
-
-
-func _on_area_2d_body_exited(body):
-	if body is CharacterBody2D:
-		$Label.hide()
-		player_inside = false
-
-
-func _input(event):
-	if event.is_action_pressed("ui_accept") and player_inside:
-		var data = {"id": object_id, "frame_id" : $Graves.frame }
-		get_parent().get_parent().decoration_show(data)
 
 func assign_image_by_id(texture_id: int):
 	if texture_id <= 4:
 		$Graves.frame = texture_id
 	else:
 		print("Error: ID de textura '", texture_id, "' fuera de rango.")
+
+
+func _on_button_pressed():
+	var data = {"id": object_id, "frame_id" : $Graves.frame }
+	get_parent().get_parent().decoration_show(data)
+
+
+func _on_area_2d_mouse_entered():
+	$Button.show()
+
+
+func _on_area_2d_mouse_exited():
+	$Button.hide()
